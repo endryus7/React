6d16b1270673b2ef101a818b6c33d5b8c568dfa1
Endryus-Montezano <endryusmontezano@live.com>
2025-06-28 18:46:25 -0300

    Trabalhando com Hooks Básicos
    Terminado

--- dio/src/pages/login/index.jsx
+++ dio/src/pages/login/index.jsx
@@ -1,27 +1,71 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo, useCallback } from 'react'
 
 const Teste = () => {
 
-    //useState
+    /* useState
     const [name, setName] = useState('Endryus');
 
     const handleChangeName = () => {
         setName(prev => prev === 'Endryus' ? 'José' : 'Endryus')
     }
+    */
 
-    //useEffect
+    /* useEffect
     useEffect(()=>{
-        alert('renderizei')
-    },[name])
+        alert('renderizei');
+        handleChangeName();
+        
+    },[])
+    */
+
+    /* useMemo
+    const calculo = useMemo (() => {
+        console.log('calculou')
+        return 10 * 26589
+    }, [])
+    console.log('renderizou', calculo)
+    */
+
+    // useMemo = referencia em variaveis(const, var let)
+    //useCallback = referencia de função.
+
+
+    // useCallback
+    const [name, setName] = useState('Endryus');
+    const [age, setAge] = useState(26);
+
+    const handleChangeName = useCallback(() => {
+        console.log('alterou nome')
+        setName(prev => prev === 'Endryus' ? 'José' : 'Endryus');
+    }, [])
+
+    const handleChangeAge = useCallback(() => {
+        const newAge = 10 * age;
+        console.log('age atual', age, newAge)
+        setAge(prev => prev === 26 ? 32 : 26);
+    }, [age])
+
+    const calculo = useMemo (() => {
+        console.log('calculou')
+        return 10 * 26589
+    }, []);
+
 
   return (
     <div>
         <p>
-            {name}
+            Nome: {name}
         </p>
-        <button onClick={handleChangeName}>Alterar</button>
+        <br />
+        <p>
+            Idade:{age}
+        </p>
+        <br />
+        <button onClick={handleChangeName}>Alterar nome</button>
+        <br />
+        <button onClick={handleChangeAge}>Alterar idade</button>
     </div>
   )
-}
+};
 
 export { Teste }
